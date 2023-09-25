112a112bba11aa94277fa7c6ba83ed7363f7e2ab
Adrian Juan <adrian_juan99@yahoo.com>
2023-09-25 19:30:07 +0800

    Created a functioning API Weather App

---
+++ main.js
@@ -0,0 +1,20 @@
+import "./style.css";
+import fetchData from "./src/helpers/fetchData";
+import displayData from "./src/helpers/displayData";
+
+const form = document.querySelector("form");
+
+async function main(e) {
+  e.preventDefault();
+  if (location === "") return;
+
+  const location = document.getElementById("search").value;
+  const apiKey = "69fee1bc80096fbb98a22448d13f3b4a";
+  const weatherApiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}`;
+  const weatherData = await fetchData(weatherApiUrl);
+  const weatherIcon = `https://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`;
+
+  displayData({ weatherData, weatherIcon });
+}
+
+form.addEventListener("submit", main);
---
+++ src/helpers/displayData.js
@@ -0,0 +1,22 @@
+export default function displayData(obj) {
+  const form = document.querySelector("form");
+  const prevArticle = document.querySelector(".card");
+  prevArticle.remove();
+
+  const article = `
+  <article class="card">
+  <div class="weather-icon-container">
+    <img id="weather-icon " src=${obj.weatherIcon} alt="" />
+  </div>
+  <div class="weather-data-container">
+    <h2 class="main-temp">${(obj.weatherData.main.temp - 257.15).toFixed(
+      2
+    )}°C</h2>
+    <p class="weather-main">${obj.weatherData.weather[0].main}</p>
+    <p class="date">${new Date()}</p>
+  </div>
+</article >
+  `;
+
+  form.innerHTML += article;
+}
